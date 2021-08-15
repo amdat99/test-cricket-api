cc486504d932d2567e0a7b89fba4245801825611
amdat99 <aamiradat99@gmail.com>
2021-08-16 00:50:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {data} from './data';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500,
+    minWidth:300,
+    marginRight:20,
+  },
+  media1: {
+    height: 20,
+  },
+  media2: {
+    height: 20,
+  }
+});
+
+
+
 
 function App() {
+  console.log(data)
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      {data.map(d => {
+        return <Card key={d.match_id}className={classes.root}>
+        <CardActionArea>
+        <Typography gutterBottom variant="h5" component="h5">
+            {d.series}
+            </Typography>
+         
+          <img
+            className={classes.media}
+            src={d.team_a_img}
+            width='100px'
+          />
+              <Typography gutterBottom variant="p" component="p">
+            {d.team_a}       
+            {d.team_a_score?
+                 <Typography  variant="p" component="p">
+                -score: {d.team_a_score.score}
+                -wickets{d.team_a_score.wicket}
+            </Typography>
+      :null}
+         { d.team_a_score&&d.team_a_score.won?
+           <span style={{color:'green'}} >winner</span>
+            :null}
+            </Typography>
+            <img
+            className={classes.media}
+            src={d.team_b_img}
+            width='100px'
+            
+          />
+              <Typography  gutterBottom variant="p" component="p">
+            {   d.team_b}
+            </Typography>
+            {d.team_b_score?
+                 <Typography  variant="p" component="p">
+                       -score: {d.team_b_score.score}
+                  {'  '}wickets: {d.team_b_score.wicket}
+            </Typography>
+      
+      :null}
+          { d.team_b_score&&d.team_b_score.won?
+              <span style={{color:'green'}} >winner</span>
+            :null}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+            {d.match_status}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {d.venue}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {d.match_type}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {d.matchs}
+            </Typography>
+            <Typography gutterBottom variant="p" component="p">
+            {d.match_date}
+            </Typography>
+            <Typography gutterBottom variant="p" component="p">
+            {d.time}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            more
+          </Button>
+          <Button size="small" color="primary">
+             More
+          </Button>
+        </CardActions>
+      </Card>
+})
+      }
+  
     </div>
   );
 }
